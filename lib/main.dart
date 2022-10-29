2192fd24e2ebc23578368d4723ca4535a3799d94
Aditya Kaushik <adityakaushik373@gmail.com>
2022-10-29 15:27:18 +0530

    themes commited

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_app/pages/home_page.dart';
 import 'package:first_app/pages/login_page.dart';
 import 'package:first_app/utils/routes.dart';
+import 'package:first_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -16,11 +17,9 @@ class Myapp extends StatelessWidget {
     int day = 1;
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily),
+      theme: Mythemes.lightTheme(context),
       // debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      darkTheme: Mythemes.darkTheme(context),
       initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => Loginpage(),
